68e38b3c5aa669aea065d8d778e1c2ea41ec387b
Eric Rasmussen <erasmussen@eaglecrk.com>
2018-04-04 17:00:02 -0500

    updates

--- course/gbcomments.php
+++ course/gbcomments.php
@@ -67,7 +67,7 @@
 					if ($_POST['useridtype']==0) {
 						//DB $query .= "imas_users.SID='{$data[$usercol]}'";
 						$query .= "imas_users.SID=:SID";
-						$qarr[':SID'] = $data[$usercol];
+						$qarr[':SID'] = Sanitize::stripHtmlTags($data[$usercol]);
 					} else if ($_POST['useridtype']==1) {
 						list($last,$first) = explode(',',$data[$usercol]);
 						$first = str_replace(' ','',$first);
@@ -90,11 +90,11 @@
 						if ($comtype=='stu') {
 							//DB $query = "UPDATE imas_students SET gbcomment='{$data[$scorecol]}' WHERE userid='$cuserid' AND courseid='$cid'";
 							$stm = $DBH->prepare("UPDATE imas_students SET gbcomment=:gbcomment WHERE userid=:userid AND courseid=:courseid");
-							$stm->execute(array(':gbcomment'=>$data[$scorecol], ':userid'=>$cuserid, ':courseid'=>$cid));
+							$stm->execute(array(':gbcomment'=>Sanitize::stripHtmlTags($data[$scorecol]), ':userid'=>$cuserid, ':courseid'=>$cid));
 						} else if ($comtype=='instr') {
 							//DB $query = "UPDATE imas_students SET gbinstrcomment='{$data[$scorecol]}' WHERE userid='$cuserid' AND courseid='$cid'";
 							$stm = $DBH->prepare("UPDATE imas_students SET gbinstrcomment=:gbinstrcomment WHERE userid=:userid AND courseid=:courseid");
-							$stm->execute(array(':gbinstrcomment'=>$data[$scorecol], ':userid'=>$cuserid, ':courseid'=>$cid));
+							$stm->execute(array(':gbinstrcomment'=>Sanitize::stripHtmlTags($data[$scorecol]), ':userid'=>$cuserid, ':courseid'=>$cid));
 						}
 						//DB mysql_query($query) or die("Query failed : " . mysql_error());
 						$successes++;
